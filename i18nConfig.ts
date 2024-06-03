d1ac62e3d220072befee8227aecc7e2fcd878227
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2024-06-03 23:20:09 +0300

    update files

---
+++ i18nConfig.ts
@@ -0,0 +1,7 @@
+const i18nConfig = {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en'
+  };
+  
+// module.exports = i18nConfig;
+export default i18nConfig;
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import getTranslations from "../i18n";
+import { LocalProps } from "../interfaces/local.props.interface";
 
-export default function Home() {
+export default async function Home({ params: { locale } }: LocalProps) {
+  const { t } = await getTranslations(locale, ["fields"]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1>{t("app-name")}</h1>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
